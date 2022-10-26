b660405a754472254ed6f8de0f48d9ec662f590f
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-26 14:49:21 +0200

    end vue-hello

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/* 
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+*/
+
+const { createApp } = Vue;
+
+    createApp({
+        data() {
+            return {
+                message: 'Hello World!',
+            }
+        }
+    }).mount('#app');
